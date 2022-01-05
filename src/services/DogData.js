28d7c481f612b72e4b30a0696a649d0fd66d3bd9
Violet Stewart <vkazoo2@gmail.com>
2022-01-05 15:19:34 -0800

    wrote functions to add delete and update dog

--- src/services/DogData.js
+++ src/services/DogData.js
@@ -10,6 +10,20 @@ export async function getDog(id) {
   return checkError(request);
 }
 
+export async function updateDog(dog) {
+  let request = await client.from('dog-data').update(dog).eq('id, dog.id');
+  return checkError(request);
+}
+
+export async function insertDog(dog) {
+  let request = await client.from('dog-data').insert(dog);
+  return checkError(request);
+}
+
+export async function deleteDog(id) {
+  let request = await client.from('dog-data').delete().match({ id });
+  return checkError(request);
+}
 //get dog by id
 
 // get all dogs
