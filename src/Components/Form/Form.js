6c0ea4c34bba99f1183d819f062761886571d4d7
Violet Stewart <vkazoo2@gmail.com>
2022-01-05 15:42:06 -0800

    set up form for inputs

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,5 +1,26 @@
 import React from 'react';
 
 export default function Form() {
-  return <div></div>;
+  return (
+    <form>
+      <label>Name
+        <input value={''} onClick={} type='text' placeholder='Name'/>
+      </label>
+      <label>Bio
+        <input value={''} onClick={} type='text' placeholder='Bio'/>
+      </label>
+      <label>Age
+        <input value={''} onClick={} type='number' placeholder='Age'/>
+      </label>
+      <label>Image
+        <input value={''} onClick={} type='text' placeholder='image'/>
+      </label>
+      <label>Breed
+        <input value={''} onClick={} type='text' placeholder='Breed'/>
+      </label>
+      
+
+      <button>Submit</button>
+    </form>
+  );
 }
