71488ead2efcce278d220877bf020b1efd389a02
Violet Stewart <vkazoo2@gmail.com>
2022-01-04 17:26:46 -0800

    set up browser router in app js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home/Home';
+import DogDetail from './Components/DogDetail/DogDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
       </header>
-    </div>
+
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dogs/:id" component={DogDetail} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
