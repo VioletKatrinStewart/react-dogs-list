31cb327e9571972c83677b2cde30074dbc2a90d5
Violet Stewart <vkazoo2@gmail.com>
2022-01-06 14:48:04 -0800

    dogs render on page and are clickable

--- src/Components/DogList/DogList.js
+++ src/Components/DogList/DogList.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default function DogList() {
-  return <div></div>;
+export default function DogList({ dogs }) {
+  console.log(dogs);
+  return (
+    <div>
+      {dogs.map((dog) => (
+        <Link key={dogs.id} to={`/dogs/${dogs.id}`}>
+          {dog.name}
+          <img src={dog.image} />
+        </Link>
+      ))}
+    </div>
+  );
 }
--- src/Views/Dogs/Dogs.js
+++ src/Views/Dogs/Dogs.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../../Components/Header/Header';
+import { getDogs } from '../../services/DogData';
+import DogList from '../../Components/DogList/DogList';
 
 export default function Dogs() {
+  const [dogs, setDogs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await getDogs();
+      setDogs(data);
+      setLoading(false);
+    };
+    getData();
+  }, []);
+  if (loading) return <h1>loading</h1>;
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <DogList dogs={dogs} />
     </div>
   );
 }
